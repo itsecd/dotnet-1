564533198e4e9a17441246885cfd19d0a44b21fe
Dryg1214 <pakazancev901@gmail.com>
2022-02-07 23:15:34 +0400

    initialization

---
+++ Lab_1/Lab_1_Indust_Pr/Lab_1_Indust_Pr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_1_Indust_Pr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
