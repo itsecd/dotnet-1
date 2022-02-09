51925bdeaeba6afd496c4b358beb9291d33d030c
Ilya <sleepvalera@gmail.com>
2022-02-09 21:53:14 +0400

    add gitignore

---
+++ src/VolumetricFigures/VolumetricFigures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VolumetricFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
