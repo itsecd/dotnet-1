e9cc30415c5881d1649fac927e8d2e734c4170c5
Divanchik <d-sh-2016@list.ru>
2022-02-13 13:09:37 +0400

    попытка начать новый проект

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+
+namespace dotnet_1.Matrix
+{
+    public class BufferedMatrix
+    {
+
+    }
+
+    public class SparseMatrix
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using dotnet_1.Matrix;
+
+namespace dotnet_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
