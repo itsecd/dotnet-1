74d04c774e2ba30e5a15c0f988fd0f675be8ebb8
Divanchik <d-sh-2016@list.ru>
2022-02-17 04:59:49 +0400

    Переделал структуру классов

---
+++ Lab1/BufferedMatrix.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+
+namespace Lab1
+{
+    public class BufferedMatrix : Matrix
+    {
+        private double[] A;
+        public override double this[int y, int x]
+        {
+            get => A[y * Height + x];
+            set {A[y * Height + x] = value;}
+        }
+        public override int Width { get; }
+        public override int Height { get; }
+
+        public BufferedMatrix(int H, int W)
+        {
+            Height = H;
+            Width = W;
+            A = new double[H*W];
+        }
+
+        public override double Norm()
+        {
+            double res = Math.Abs(A[0]);
+            foreach (double i in A)
+                if (Math.Abs(i) > res)
+                    res = Math.Abs(i);
+            return res;
+        }
+
+        public override double NormL()
+        {
+            double[] n = new[] {Math.Abs(A.Max()), Math.Abs(A.Min())};
+            return n.Max();
+        }
+
+        public override string ToString() => $"BufferedMatrix [[{Height}x{Width}]]";
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType())
+                return false;
+            var tmp = obj as BufferedMatrix;
+            if (tmp.Width != Width || tmp.Height != Height)
+                return false;
+            return A.SequenceEqual(tmp.A);
+        }
+        public override int GetHashCode() => HashCode.Combine(A, Height, Width);
+    }
+}
--- Lab1/Matrix.cs
+++ Lab1/Matrix.cs
@@ -4,89 +4,16 @@ using System.Linq;
 
 namespace Lab1
 {
-    public class BufferedMatrix
+    public abstract class Matrix
     {
-        protected double[,] A;
-        int Width { get; }
-        int Height { get; }
+        public abstract int Width { get; }
+        public abstract int Height { get; }
 
-        public BufferedMatrix(int w, int h)
-        {
-            Width = w;
-            Height = h;
-            A = new double[Height, Width];
-        }
-
-        public double Get(int x, int y) => A[x, y];
-
-        public void Set(int x, int y, double val)
-        {
-            A[x, y] = val;
-        }
-
-        public double Norm
-        {
-            get
-            {
-                double maxval = Math.Abs(A[0, 0]);
-                for (int i = 0; i < Height; i++)
-                    for (int j = 0; j < Width; j++)
-                        if (Math.Abs(A[i, j]) > maxval)
-                            maxval = Math.Abs(A[i, j]);
-                return maxval;
-            }
-        }
-
-        public double NormL()
-        {
-            return 0;
-        }
-
-        public override string ToString()
-        {
-            return $"BufferedMatrix [[{Height} x {Width}]]";
-        }
-
-        public override bool Equals(object obj)
-        {
-            bool eq = obj.GetType() != this.GetType();
-            if (eq)
-                return false;
-            BufferedMatrix tmp = obj as BufferedMatrix;
-            return this == tmp;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(A, Width, Height);
-        }
-
-        public static bool operator ==(BufferedMatrix lhs, BufferedMatrix rhs)
-        {
-            if (lhs.Width != rhs.Width || lhs.Height != rhs.Height)
-                return false;
-            for (int i = 0; i < lhs.Height; i++)
-                for (int j = 0; j < lhs.Width; j++)
-                    if (lhs.Get(i, j) != rhs.Get(i, j))
-                        return false;
-            return true;
-        }
-
-        public static bool operator !=(BufferedMatrix lhs, BufferedMatrix rhs)
-        {
-            if (lhs.Width != rhs.Width || lhs.Height != rhs.Height)
-                return true;
-            for (int i = 0; i < lhs.Height; i++)
-                for (int j = 0; j < lhs.Width; j++)
-                    if (lhs.Get(i, j) != rhs.Get(i, j))
-                        return true;
-            return false;
-        }
-
-    }
-
-    public class SparseMatrix
-    {
-
+        public abstract double this[int x, int y] {get; set;}
+        public abstract double Norm();
+        public abstract double NormL();
+        public override string ToString() => $"Matrix [[{Height}x{Width}]]";
+        public override bool Equals(object obj) => base.Equals(obj);
+        public override int GetHashCode() => base.GetHashCode();
     }
 }
