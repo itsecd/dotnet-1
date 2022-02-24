55043a015b08c65306b85fde73ac3a16950c81b6
Ivan <pravdin.biss@gmail.com>
2022-02-24 14:04:39 +0400

    update Program.cs

--- src/lab1/lab1/Program.cs
+++ src/lab1/lab1/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using lab1.Commands;
+using lab1.Infrastructure;
+using lab1.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console.Cli;
 
 namespace lab1
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IBinaryOperationsRepository, XmlBinaryOperationsRepository>();
+
+            var registrar = new TypeRegistrar(serviceCollection);
+            var app = new CommandApp(registrar);
+
+            app.Configure(config =>
+            {
+                config.AddCommand<AddBinaryOperation>("add");
+                config.AddCommand<RemoveBinaryOperation>("remove");
+                config.AddCommand<ShowAllBinaryOperation>("show");
+                config.AddCommand<CompareBinaryOperation>("compare");
+                config.AddCommand<MinBinaryOperation>("min");
+            });
+
+            app.Run(args);
         }
     }
 }
