84c5b5eadcbf407c5ea7b0fda9ccaff0d8ad68c9
Ivan <pravdin.biss@gmail.com>
2022-02-24 14:04:01 +0400

    add Repositories

---
+++ src/lab1/lab1/Repositories/IBinaryOperationsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1.Repositories
+{
+    interface IBinaryOperationsRepository
+    {
+        public void AddOperation(int index, BinaryOperation operation);
+        public void RemoveOperation(int index);
+        public void RemoveAll();
+
+        public List<BinaryOperation> GetAll();
+    }
+}
---
+++ src/lab1/lab1/Repositories/XmlBinaryOperationsRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace lab1.Repositories
+{
+    class XmlBinaryOperationsRepository : IBinaryOperationsRepository
+    {
+        private const string _filePath = "database.xml";
+
+        private List<BinaryOperation> _operations;
+
+        private void ReadFile()
+        {
+            if (_operations != null)
+                return;
+
+            if (!File.Exists(_filePath))
+            {
+                _operations = new();
+                return;
+            }
+
+            var xmlSerialazer = new XmlSerializer(typeof(List<BinaryOperation>));
+            using var fileStream = new FileStream(_filePath, FileMode.Open);
+            _operations = (List<BinaryOperation>)xmlSerialazer.Deserialize(fileStream);
+        }
+
+        private void WriteFile()
+        {
+            var xmlSerialazer = new XmlSerializer(typeof(List<BinaryOperation>));
+            using var fileStream = new FileStream(_filePath, FileMode.Create);
+            xmlSerialazer.Serialize(fileStream,_operations);
+        }
+
+        public void AddOperation(int index, BinaryOperation operation)
+        {
+            ReadFile();
+            _operations.Insert(index,operation);
+            WriteFile();
+        }
+
+        public void RemoveOperation(int index)
+        {
+            ReadFile();
+            _operations.RemoveAt(index);
+            WriteFile();
+        }
+
+        public void RemoveAll()
+        {
+            ReadFile();
+            _operations.RemoveAll( _ =>  true );
+            WriteFile();
+        }
+
+        public List<BinaryOperation> GetAll()
+        {
+            ReadFile();
+            return _operations;
+        }
+    }
+}
