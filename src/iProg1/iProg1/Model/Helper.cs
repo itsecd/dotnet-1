2b13b06dbf32abf275c2370f7839d3ef2758489f
putilove <gleb89047408502@gmail.com>
2022-02-24 14:13:00 +0400

    add help classes

---
+++ src/iProg1/iProg1/Model/Helper.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Xml;
+
+namespace iProg1.Model
+{
+    public static class Helper
+    {
+        public static bool IsValidIndex(int index, int maxIndex)
+        {
+            return index < maxIndex;
+        }
+        
+        public static void SkipToElement(XmlTextReader reader)
+        {
+            while (reader.NodeType != XmlNodeType.Element)
+            {
+                reader.Read();
+            }
+        }
+        
+        public static string GetCurrentElemsOfMatrix(double[][] array, int indI, int indJ)
+        {
+            var str = new StringBuilder("");
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[0].Length; j++)
+                {
+                    if (i == indI && j == indJ)
+                    {
+                        return str.ToString();
+                    }
+                    else
+                    {
+                        str.Append(array[i][j]);
+                        str.Append(" ");
+                    }
+                }
+                str.Append('\n');
+            }
+            return str.ToString();
+        }
+    }
+}
