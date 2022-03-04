480839de6ef2513ea42c9cc34c3088ee78739c64
79277 <79277@LAPTOP-TREROOEH>
2022-03-04 11:43:21 +0400

    last

--- src/PromProg1/PromProg1/Program.cs
+++ src/PromProg1/PromProg1/Program.cs
@@ -8,42 +8,7 @@ namespace PromProg1
     {
         static void Main(string[] args)
         {
-            //FigureRepository figureRepository= new FigureRepository();
-            // figureRepository.AddFigure();
-            // var figureType = AnsiConsole.Prompt(new SelectionPrompt<string>().Title)
-            //var figureType = AnsiConsole.Prompt(new SelectionPrompt<string>()
-            //                   .Title("What to add?")
-            //                   .AddChoices("Rectangle", "Circle", "Triangle"));
-            //Figure figure = figureType switch
-            //{
-            //    "Rectangle" => new Rectangle(
-            //        new Point(AnsiConsole.Prompt(new TextPrompt<double>("Точка X1 :")),
-            //        AnsiConsole.Prompt(new TextPrompt<double>("Точка Y1 :"))),
-            //        new Point(AnsiConsole.Prompt(new TextPrompt<double>("Точка X2 :")),
-            //        AnsiConsole.Prompt(new TextPrompt<double>("Точка Y2 :")))
-            //        ),
-            //     "Circle" => new Circle(
-            //        new Point(AnsiConsole.Prompt(new TextPrompt<double>("Точка X1 :")),
-            //        AnsiConsole.Prompt(new TextPrompt<double>("Точка Y1 :"))),
-            //        AnsiConsole.Prompt(new TextPrompt<double>("Radius :"))),
-
-            //      "Triangle" => new Triangle(
-            //        new Point(AnsiConsole.Prompt(new TextPrompt<double>("Точка X1 :")),
-            //        AnsiConsole.Prompt(new TextPrompt<double>("Точка Y1 :"))),
-            //        new Point(AnsiConsole.Prompt(new TextPrompt<double>("Точка X2 :")),
-            //        AnsiConsole.Prompt(new TextPrompt<double>("Точка Y2 :"))),
-            //         new Point(AnsiConsole.Prompt(new TextPrompt<double>("Точка X3 :")),
-            //        AnsiConsole.Prompt(new TextPrompt<double>("Точка Y3 :")))),
-            //        _ => null
-            //};
-            //if(figure == null)
-            //{
-            //    AnsiConsole.MarkupLine("Неизвестный тип фигуры: {figureType}[/]");
-            //}
-            //figureRepository.AddFigure(figure);
-            //var table = new Table();
-            //table.AddColumn("Type");
-            //table.AddColumn("Coordinate");
+            
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddSingleton<IFigureRepository, FigureRepository>();
 
@@ -55,7 +20,6 @@ namespace PromProg1
                 config.AddCommand<AddFigureCommand>("add");
                 config.AddCommand<DeleteCommand>("delete");
                 config.AddCommand<DeleteAllCommand>("deleteall");
-                config.AddCommand<FileCommands>("file");
                 config.AddCommand<TableCommand>("table");
                 config.AddCommand<SumCommands>("sum");
                 config.AddCommand<CompareCommand>("compare");
