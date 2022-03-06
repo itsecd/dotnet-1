fc435c2b02110da00ffe3b2c6c8ab9c70180345a
LONHik4 <gaynullin.dmitriy@gmail.com>
2022-03-06 21:04:50 +0400

    Создано меню и добавлен вывод данных в консоль

--- src/lab1/lab1/Program.cs
+++ src/lab1/lab1/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using lab1.Functions;
 
 namespace lab1
 {
@@ -6,7 +9,58 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string textMenu;
+            var table = new Table();
+            table.AddColumn("Type");
+            table.AddColumn("Variables");
+            table.AddColumn("Сalculated value");
+            table.AddColumn("Derivative ");
+            List<Function> functionList = new List<Function>();
+            var menu = AnsiConsole.Prompt(new SelectionPrompt<string>()
+            .Title("Choose your next action:")
+            .PageSize(10)
+            .AddChoices("Function selection"));
+            switch (menu)
+            {
+                case "Function selection":
+                    textMenu = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                        .Title("Choose a function type")
+                        .PageSize(10)
+                        .AddChoices("Constant", "Power Function",
+                        "Exponential Function", "LogarithmicFunction"));
+                    Function function = textMenu switch
+                    {
+                        "Constant" => new Constant(new Data(1,
+                          AnsiConsole.Ask<int>("Enter a number"))),
+
+                        "Power Function" => new PowerFunction(new Data(
+                          AnsiConsole.Ask<int>("Enter a variable"),
+                          AnsiConsole.Ask<int>("Enter a coefficient"),
+                          AnsiConsole.Ask<int>("Enter a degree"))),
+
+                        "Exponential Function" => new ExponentialFunction(new Data(
+                          AnsiConsole.Ask<int>("Enter a variable"),
+                          AnsiConsole.Ask<int>("Enter a coefficient"),
+                          AnsiConsole.Ask<int>("Enter a function base"))),
+
+                        "Logarithmic Function" => new LogarithmicFunction(new Data(
+                          AnsiConsole.Ask<int>("Enter a variable"),
+                          AnsiConsole.Ask<int>("Enter a coefficient"),
+                          AnsiConsole.Ask<int>("Enter a base of the logarithm"))),
+
+                        _ => throw new NotImplementedException()
+                    };
+                    AnsiConsole.Clear();
+                    functionList.Add(function);
+                    foreach (var function1 in functionList)
+                    {
+                        table.AddRow(function1.GetType().Name, function1.ToString(),
+                            function1.Calculation().ToString(), function1.Derivative().ToString());
+                    }
+                    AnsiConsole.Write(table);
+                    break;
+            }
         }
     }
 }
