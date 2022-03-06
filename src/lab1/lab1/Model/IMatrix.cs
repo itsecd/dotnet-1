dd9506a3c381f573c3a1a7080f56627751f863e0
danila <danila_glazkov@mail.ru>
2022-03-06 21:11:25 +0400

    add matrix interface

---
+++ src/lab1/lab1/Model/IMatrix.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace lab1.Model
+{
+    internal interface IMatrix
+    {
+        double GetValue(int height, int width);
+        void SetValue(int height, int width, double value);
+        double GetAbsMax();
+    }
+}
