97ac03234fd1345790cdac702fc201507a309a4e
Altolele <89626132692@mail.ru>
2022-03-15 22:59:09 +0400

    Реализован Repositories

---
+++ src/Lab1/Lab1/Repositories/FiguresRepository.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using Lab1.Model;
+
+namespace Lab1.Repositories
+{
+    internal class FiguresRepository: IFiguresRepository
+    {
+
+        private const string StorageFileName = "figures.xml";
+        private List<Figure> _figures;
+        private void ReadFromFile()
+        {
+            if (_figures != null) return;
+            if (!File.Exists(StorageFileName))
+            {
+                _figures = new List<Figure>();
+                return;
+            }
+            var xmlDeserializer = new XmlSerializer(typeof(List<Figure>));
+            using var fileStream = new FileStream(StorageFileName, FileMode.Open);
+            _figures = (List<Figure>)xmlDeserializer.Deserialize(fileStream);
+        }
+        private void WriteToFile()
+        {
+            var xmlSerializer = new XmlSerializer(typeof(List<Figure>));
+            using var fileStream = new FileStream(StorageFileName, FileMode.Create);
+            xmlSerializer.Serialize(fileStream, _figures);
+        }
+        public void AddFigure(Figure figure, int index)
+        {
+            if (figure == null)
+                throw new ArgumentNullException(nameof(figure));
+            ReadFromFile();
+            _figures.Insert(index, figure);
+            WriteToFile();
+        }
+        public void RemoveFigure(int index)
+        {
+            ReadFromFile();
+            _figures.RemoveAt(index);
+            WriteToFile();
+        }
+        public void RemoveAllFigures()
+        {
+            ReadFromFile();
+            _figures.Clear();
+            WriteToFile();
+        }
+        public bool CompareFigures(int firstIndex, int secondIndex)
+        {
+            ReadFromFile();
+            return _figures[firstIndex].Equals(_figures[secondIndex]);
+        }
+        public Rectangle GetMinRectangle(int index)
+        {
+            ReadFromFile();
+            return _figures[index].GetMinRectangle();
+        }
+        public double SumArea()
+        {
+            ReadFromFile();
+            double sum = 0;
+            foreach (var figure in _figures)
+            {
+                sum += figure.GetArea();
+            }
+            return sum;
+        }
+        public double SumAreaLinq()
+        {
+            ReadFromFile();
+            double sum = _figures.Sum(figure => figure.GetArea());
+            return sum;
+        }
+        public Figure GetFigure(int index)
+        {
+            ReadFromFile();
+            return _figures[index];
+        }
+        public List<Figure> GetFigures()
+        {
+            ReadFromFile();
+            return _figures;
+        }
+        public int GetCountFigures()
+        {
+            ReadFromFile();
+            return _figures.Count;
+        }
+    }
+}
---
+++ src/Lab1/Lab1/Repositories/IFiguresRepository.cs
@@ -0,0 +1,19 @@
+﻿using Lab1.Model;
+using System.Collections.Generic;
+
+namespace Lab1.Repositories
+{
+    internal interface IFiguresRepository
+    {
+        void AddFigure(Figure figure, int index);
+        void RemoveFigure(int index);
+        void RemoveAllFigures();
+        bool CompareFigures(int firstIndex, int secondIndex);
+        Rectangle GetMinRectangle(int index);
+        double SumArea();
+        double SumAreaLinq();
+        Figure GetFigure(int index);
+        List<Figure> GetFigures();
+        int GetCountFigures();
+    }
+}
