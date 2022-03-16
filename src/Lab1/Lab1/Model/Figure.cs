f2e026485a559592f66d3342ceefc37ca543e543
Altolele <89626132692@mail.ru>
2022-03-16 17:26:20 +0400

    fix

--- src/Lab1/Lab1/Model/Figure.cs
+++ src/Lab1/Lab1/Model/Figure.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Xml.Serialization;
 
 namespace Lab1.Model
 {
+    [XmlInclude(typeof(Rectangle))]
+    [XmlInclude(typeof(Triangle))]
+    [XmlInclude(typeof(Circle))]
     public abstract class Figure
     {
         public abstract double GetPerimeter();
