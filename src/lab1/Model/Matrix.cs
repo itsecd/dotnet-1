1cc752bb7892767062d38b08c7b1a2bf58c39802
danila <danila_glazkov@mail.ru>
2022-03-23 02:47:41 +0400

    some fix

--- src/lab1/Model/Matrix.cs
+++ src/lab1/Model/Matrix.cs
@@ -4,6 +4,8 @@ namespace lab1.Model
 {
     public interface IMatrix
     {
+        public int Width { get; init; }
+        public int Height { get; init; }
         public double GetValue(int i, int j);
         public void SetValue(int i, int j, double value);
         public double GetAbsMax();
