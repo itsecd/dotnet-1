7c490c2bf0e674e8acfd64d11cf557f629e4be86
danila <danila_glazkov@mail.ru>
2022-03-22 16:49:45 +0400

    add repository interface

---
+++ src/lab1/Repositories/IMatrixRepository.cs
@@ -0,0 +1,15 @@
+﻿using lab1.Model;
+using System.Collections.Generic;
+
+namespace lab1.Repositories
+{
+    interface IMatrixRepository
+    {
+        public bool Compare(int lhs, int rhs);
+        public void Insert(int index, IMatrix? matrix);
+        public void RemoveAt(int index);
+        public void Clear();
+        public IMatrix GetMatrix(int index);
+        public List<IMatrix> GetAll();
+    }
+}
