1fefe4d701974cc7db00bf45b8c08f3dd52a72cc
Arisa <uliannaobr@gmail.com>
2022-04-12 14:18:36 +0400

    Tests Model Functions

--- Sources/Lab1/Program.cs
+++ Sources/Lab1/Program.cs
@@ -7,12 +7,34 @@ namespace Lab1
         static void Main(string[] ars)
         {
             var test1 = new ConstantFunction(12);
+            /*
             var test2 = new LinearFunction(1, 2);
-            Console.WriteLine(test1.Constant);
-            Console.WriteLine(test2.Linear.ToString());
-            Console.WriteLine(test2.Constant.ToString());
-        }
+            var test3 = new QuadraticFunction(1, 2, 3);
+            var test4 = new SinusFunction(-1, 2, 0);
+            var test5 = new CosinusFunction(1, 2, 0);
+            Console.WriteLine("\n Entered Function: \n");
+            Console.WriteLine(test1.ToString());
+            Console.WriteLine(test2.ToString());
+            Console.WriteLine(test3.ToString());
+            Console.WriteLine(test4.ToString());
+            Console.WriteLine(test5.ToString());
+            Console.WriteLine("\n The Derivative Function bu Entered Function: \n");
+            Console.WriteLine(test1.GetDerivative().ToString());
+            Console.WriteLine(test2.GetDerivative().ToString());
+            Console.WriteLine(test3.GetDerivative().ToString());
+            Console.WriteLine(test4.GetDerivative().ToString());
+            Console.WriteLine(test5.GetDerivative().ToString());
+            Console.WriteLine("\n The Calculated Function bu Entered Function with number 10: \n");
+            Console.WriteLine(test1.Calculate(10).ToString());
+            Console.WriteLine(test2.Calculate(10).ToString());
+            Console.WriteLine(test3.Calculate(10).ToString());
+            Console.WriteLine(test4.Calculate(10).ToString());
+            Console.WriteLine(test5.Calculate(10).ToString());
+            Console.WriteLine("\n The Equals Function bu Entered Function: \n");
+            Console.WriteLine(test4.Equals(test5).ToString());
+            */
 
+        }
     }
 }
 
