7e8790af77468a683f97ce9ff2de17a7c550d748
LONHik4 <gaynullin.dmitriy@gmail.com>
2022-05-06 00:28:17 +0400

    Добавлен Xml репозиторий

---
+++ src/lab1/lab1/Repositories/IFunctionRepository.cs
@@ -0,0 +1,14 @@
+﻿using lab1.Functions;
+using System.Collections.Generic;
+
+namespace lab1.Repositories
+{
+    interface IFunctionRepository
+    {
+        public void AddFunction(int index, Function operation);
+        public void RemoveFunction(int index);
+        public void RemoveAll();
+
+        public List<Function> GetAll();
+    }
+}
---
+++ src/lab1/lab1/Repositories/XmlFunctionRepository.cs
@@ -0,0 +1,64 @@
+﻿using lab1.Functions;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace lab1.Repositories
+{
+    class XmlFunctionRepository : IFunctionRepository
+    {
+        private List<Function> _functions;
+        private const string _filePath = "functions.xml";
+
+        private void ReadFile()
+        {
+            if (_functions != null)
+                return;
+
+            if (!File.Exists(_filePath))
+            {
+                _functions = new();
+                return;
+            }
+
+            var xmlSerializer = new XmlSerializer(typeof(List<Function>));
+            using var fileStream = new FileStream(_filePath, FileMode.OpenOrCreate);
+            _functions = (List<Function>)xmlSerializer.Deserialize(fileStream);
+        }
+
+        private void WriteFile()
+        {
+            var xmlSerializer = new XmlSerializer(typeof(List<Function>));
+            using var fileStream = new FileStream(_filePath, FileMode.OpenOrCreate);
+            xmlSerializer.Serialize(fileStream, _functions);
+        }
+
+        public List<Function> GetAll()
+        {
+            ReadFile();
+            return _functions;
+        }
+
+        public void RemoveAll()
+        {
+            ReadFile();
+            _functions.RemoveAll(_ => true);
+            WriteFile();
+        }
+
+
+        public void RemoveFunction(int index)
+        {
+            ReadFile();
+            _functions.RemoveAt(index);
+            WriteFile();
+        }
+
+        public void AddFunction(int index, Function operation)
+        {
+            ReadFile();
+            _functions.Insert(index, operation);
+            WriteFile();
+        }
+    }
+}
