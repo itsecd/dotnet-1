a5bdd9db49796dc42cda5bd3501e1db9c01df385
Roman <asd@asd.asd>
2022-05-10 23:18:08 +0400

    Add Xml storage Repository

---
+++ prProgLab1/Repository/IFuncRepository.cs
@@ -0,0 +1,15 @@
+﻿using prProgLab1.Model;
+using System.Collections.Generic;
+
+namespace prProgLab1.Repository
+{
+    public interface IFunctionsRepository
+    {
+        void Insert(int index, Function func);
+        void RemoveAt(int index);
+        List<Function> GetAll();
+        void Clear();
+        string MinFunc(int x);
+        string MinFuncLINQ(int x);
+    }
+}
---
+++ prProgLab1/Repository/XmlStorageRepository.cs
@@ -0,0 +1,93 @@
+﻿using prProgLab1.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace prProgLab1.Repository
+{
+    class XmlStorageRepository : IFunctionsRepository
+    {
+        private const string StorageFileName = "functions.xml";
+
+        private List<Function> _functionsList;
+
+        public void Insert(int index, Function func)
+        {
+            ReadFromFile();
+            _functionsList.Insert(index, func);
+            WriteToFile();
+        }
+
+        public void RemoveAt(int index)
+        {
+            ReadFromFile();
+            _functionsList.RemoveAt(index);
+            WriteToFile();
+        }
+
+        public List<Function> GetAll()
+        {
+            ReadFromFile();
+            return _functionsList;
+        }
+
+        public void Clear()
+        {
+            ReadFromFile();
+            _functionsList.Clear();
+            WriteToFile();
+        }
+
+        public string MinFunc(int x)
+        {
+            ReadFromFile();
+            if (_functionsList.Count == 0)
+                return "";
+
+            var min = _functionsList[0].GetDerivative().GetValue(x);
+            var function = "";
+            foreach (Function func in _functionsList)
+            {
+                if (func.GetDerivative().GetValue(x) < min)
+                {
+                    min = func.GetDerivative().GetValue(x);
+                    function = func.ToString();
+                }
+            }
+            return function;
+        }
+
+
+        public string MinFuncLINQ(int x)
+        {
+            ReadFromFile();
+            var minValue = _functionsList.Min(func => func.GetDerivative().GetValue(x));
+            return _functionsList.First(func => func.GetDerivative().GetValue(x) == minValue).ToString();
+        }
+
+        private void ReadFromFile()
+        {
+            if (_functionsList != null)
+                return;
+
+            if (!File.Exists(StorageFileName))
+            {
+                _functionsList = new List<Function>();
+                return;
+            }
+
+            var xmlSerializer = new XmlSerializer(typeof(List<Function>));
+            using var fileStream = new FileStream(StorageFileName, FileMode.Open);
+            _functionsList = (List<Function>)xmlSerializer.Deserialize(fileStream);
+        }
+
+        private void WriteToFile()
+        {
+            var xmlSerializer = new XmlSerializer(typeof(List<Function>));
+            using var fileStream = new FileStream(StorageFileName, FileMode.Create);
+            xmlSerializer.Serialize(fileStream, _functionsList);
+        }
+    }
+}
