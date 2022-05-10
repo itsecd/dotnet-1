eaadce39b2c365cecd9f235d8c3079bbd8c10a3c
Sorokina6312 <baspketlayer@gmail.com>
2022-05-10 22:04:25 +0400

    fix4

--- src/Lab1_3D/Lab1_3D/Commands/TotalVolumeCollection.cs
+++ src/Lab1_3D/Lab1_3D/Commands/TotalVolumeCollection.cs
@@ -18,8 +18,8 @@ namespace Lab1_3D.Commands
         }
         public override int Execute([NotNull] CommandContext context, [NotNull] TotalVolumeCollectionSettings settings)
         {
-            AnsiConsole.Write(new Markup($"[pink]The total volume of the collection's figures:[/] [white] {_figureRepository.TotalVolume():f3}[/]\n"));
-            AnsiConsole.Write(new Markup($"[pink]\nThe total volume of the collection's figures (via System.Linq):[/] [white] {_figureRepository.TotalVolumeWithLinq():f3} [/]\n"));
+            AnsiConsole.Write(new Markup($"[fuchsia]The total volume of the collection's figures:[/] [white] {_figureRepository.TotalVolume():f3}[/]\n"));
+            AnsiConsole.Write(new Markup($"[fuchsia]\nThe total volume of the collection's figures (via System.Linq):[/] [white] {_figureRepository.TotalVolumeWithLinq():f3} [/]\n"));
             return 0;
         }
     }
